e743f62b52e5f10ed3911d5bfed1df45007335cd
jeremiahrrobinson <jeremiah.r.robinson@gmail.com>
2025-12-15 21:49:59 -0700

    Fix github pages bug

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   const base =
     env.VITE_BASE_PATH ||
-    (mode === 'production' ? '/NetworkGameShow/' : '/');
+    (mode === 'production' ? '/CS425-NetworkGameShow/' : '/');
 
   return {
     base,
